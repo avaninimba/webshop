3b36e6f0494f955a8c563d6380501fea891d792d
avaninimba <32764673+avaninimba@users.noreply.github.com>
2018-01-14 00:37:58 +0100

    fixed the produkt info

--- scriptAssignment.js
+++ scriptAssignment.js
@@ -3,7 +3,7 @@ var shoppingCart = [];
 var isItemsViewVisible = false;
 
 /* Fetch data from the json file into a javascript object */
-fetch("./assets/data.json")
+fetch("./assets/produkter.json")
 .then(function(response) {
     return response.json();
 })
@@ -34,20 +34,20 @@ function createUIFromLoadedItemsData() {
 
 function createListItem(itemData, index) {
     /* Title */
-    var title = document.createElement("h3");
-    title.innerText = itemData.title;
+    var prodName = document.createElement("h3");
+    prodName.innerText = itemData.prodName;
     
-    /* Description */
-    var description = document.createElement("p");
-    description.innerText = itemData.description;
+    /* prodDesc */
+    var prodDesc = document.createElement("p");
+    prodDesc.innerText = itemData.prodDesc;
     
     /* Image */
     var image = document.createElement("img");
     image.src = "./assets/" + itemData.image;
 
     /* Price */
-    var price = document.createElement("span");
-    price.innerText = "" + itemData.price + " kr";
+    var prodPrice = document.createElement("span");
+    prodPrice.innerText = "" + itemData.prodPrice + " kr";
     
     /* Button */
     var button = document.createElement("button");
@@ -59,97 +59,11 @@ function createListItem(itemData, index) {
     };
 
     var item = document.createElement("li");
-    item.appendChild(title);
-    item.appendChild(description);
+    item.appendChild(prodName);
+    item.appendChild(prodDesc);
     item.appendChild(image);
-    item.appendChild(price);
+    item.appendChild(prodPrice);
     item.appendChild(button);
 
     return item;
-}
-
-
-function showShoppingCart() {
-    if (!isItemsViewVisible) { return; }
-    isItemsViewVisible = false;
-
-    /* Header */
-    var header = document.createElement("h2");
-    header.innerHTML = '<i class="fa fa-shopping-cart" aria-hidden="true"></i>' + " Kundvagn";
-    
-    /* Shopping list */
-    var list = document.createElement("ul");
-    for(var index = 0; index < shoppingCart.length; index++) {
-        list.appendChild(createShoppingCartItem(shoppingCart[index], index));
-    }
-    
-    /* Shopping info & action */
-    var info = createShoppingSummary();
-    
-    var content = document.createElement("div");
-    content.appendChild(header);
-    content.appendChild(list);
-    content.appendChild(info);
-
-    var container = document.querySelector("#main");
-    container.replaceChild(content, container.firstChild);
-}
-
-function createShoppingCartItem(itemData, index) {
-    /* Image */
-    var image = document.createElement("img");
-    image.src = "./assets/" + itemData.image;
-
-    /* Title */
-    var title = document.createElement("h3");
-    title.innerText = itemData.title;
-
-    /* Price */
-    var price = document.createElement("span");
-    price.innerText = "" + itemData.price + " kr";
-    
-    /* Button */
-    var button = document.createElement("button");
-    button.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i>' + "&nbsp;&nbsp;&nbsp;" + "Ta bort";
-    button.onclick = function() {
-        /* Remove the item from the array */
-        shoppingCart.splice(index, 1);
-        /* Update the counter */
-        counter = document.querySelector("#counter");
-        counter.innerText = shoppingCart.length;
-        /* Update the UI list */
-        isItemsViewVisible = true;
-        showShoppingCart();
-    };
-
-    var item = document.createElement("li");
-    item.appendChild(image);
-    item.appendChild(title);
-    item.appendChild(price);
-    item.appendChild(button);
-
-    return item;
-}
-
-function createShoppingSummary() {
-    /* Total price */
-    var totalPrice = 0;
-    for(var i = 0; i < shoppingCart.length; i++) {
-        totalPrice += shoppingCart[i].price;
-    }
-    var priceLabel = document.createElement("h2");
-    priceLabel.innerText = "Totalt pris: " + totalPrice + " kr";
-    
-    /* Proceed button */
-    var proceedButton = document.createElement("button");
-    proceedButton.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>' + "&nbsp;&nbsp;&nbsp;" + "Slutför ditt köp";
-    proceedButton.onclick = function() {
-        alert("Tack för din beställning, vi önskar dig en fin kväll! Ses snart igen =)");
-    };
-
-    var info = document.createElement("div");
-    info.appendChild(priceLabel);
-    info.appendChild(proceedButton);
-
-    return info;
 }
