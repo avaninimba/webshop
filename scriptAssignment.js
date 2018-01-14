ed6243b64b06e342fd550b1e21802db91d762c8d
avaninimba <32764673+avaninimba@users.noreply.github.com>
2018-01-15 00:10:27 +0100

    trying to create product card

--- scriptAssignment.js
+++ scriptAssignment.js
@@ -3,7 +3,7 @@ var shoppingCart = [];
 var isItemsViewVisible = false;
 
 /* Fetch data from the json file into a javascript object */
-fetch("./assets/produkter.json")
+fetch("assets/produkter.json")
 .then(function(response) {
     return response.json();
 })
@@ -14,57 +14,28 @@ fetch("./assets/produkter.json")
 
 /* Use the data to create a list of these object on your website */
 function createUIFromLoadedItemsData() {
-    if (isItemsViewVisible) { return; }
-    isItemsViewVisible = true;
+    for(i = 0; i < itemsData.length; i++) {
+        
+        
+                    var produktId = (itemsData[i].id);
+                    var produktName = (itemsData[i].prodName);
+                    var produktDesc = (itemsData[i].prodDesc);
+                    var produktImage = "images/" + (itemsData[i].image);
+                    var produktPrice = (itemsData[i].prodPrice);
+                    //var produktHK = (itemsData[i].huvudKat);
+                    //var produktUK = (itemsData[i].underKat);
+                    var produktCard = "";
+                    
+                    
+                    
+        
+                    var produktCard = '<div class="col-lg-3"><div class="card"><img class="card-img-top" src="' + produktImage + '"><div class="card-body"><h4 class="card-title">' + produktName + '</h4><div class="card-footer "><p>Pris: ' + produktPrice + '</p></div></div></div></div>';
+                    $('.main').append(produktCard);
+                    
+                    
+                    
+        
+                };
+        
     
-    /* Create a list of the products */
-    var list = document.createElement("ul");
-    for(var index = 0; index < itemsData.length; index++) {
-        list.appendChild(createListItem(itemsData[index], index));
-    }
-
-    /* Add the list to the DOM */
-    var container = document.querySelector("#main");
-    if (container.firstChild) {
-        container.replaceChild(list, container.firstChild);
-    } else {
-        container.appendChild(list);
-    }
-}
-
-function createListItem(itemData, index) {
-    
-    /* Image */
-    var image = document.createElement("img");
-    image.src = "./images/" + itemData.image;
-
-     /* Title */
-     var prodName = document.createElement("h3");
-     prodName.innerText = itemData.prodName;
-     
-     /* prodDesc */
-     var prodDesc = document.createElement("p");
-     prodDesc.innerText = itemData.prodDesc;
-
-    /* Price */
-    var prodPrice = document.createElement("span");
-    prodPrice.innerText = "" + itemData.prodPrice + " kr";
-    
-    /* Button */
-    var button = document.createElement("button");
-    button.innerHTML = '<i class="fa fa-cart-arrow-down" aria-hidden="true"></i>' + "&nbsp;&nbsp;&nbsp;" + "Lägg till i kundvagnen";
-    button.onclick = function() {
-        shoppingCart.push(itemData);
-        counter = document.querySelector("#counter");
-        counter.innerText = shoppingCart.length;
-    };
-
-    var item = document.createElement("li");
-    item.appendChild(image);
-    item.appendChild(prodName);
-    item.appendChild(prodDesc);
-    item.appendChild(prodPrice);
-    item.appendChild(button);
-
-    return item;
 }
