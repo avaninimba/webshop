7cfdc75d94ef8deeffcdd41c8f5eab2deff133b1
avaninimba <32764673+avaninimba@users.noreply.github.com>
2018-01-16 00:23:59 +0100

    fixed menu

--- scriptAssignment.js
+++ scriptAssignment.js
@@ -44,10 +44,10 @@ function printHuvudKat() {
         for(i = 0; i < huvudKat.length; i++) {
 
             var huvudKatId = (huvudKat[i].id);
-            var huvudKatTea = (huvudKat[i].category);
+            var huvudKatCat = (huvudKat[i].category);
             var printHuvudKat = "";
 
-            var printHuvudKat = '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" onclick= "visaHKProdukter(' + [i] + ')">'+ huvudKat[i].category +'<span class="caret"></span></a><ul class="dropdown-menu" id="hk'+ huvudKatId +'"></ul></li>';
+            var printHuvudKat = '<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false" onclick= "visaHKProdukter(' + [i] + ')">'+ huvudKat[i].category +'</a><ul class="dropdown-menu" id="hk'+ huvudKatId +'"></ul></li>';
             $('#huvudKat').append(printHuvudKat);
 
         };
@@ -71,10 +71,12 @@ fetch("assets/underkategorier.json")
             var underKatSub = (underKat[i].subcategory);
             var underKatHuvud = (underKat[i].huvudkategori);
 
-            /*var underHuvudKat = '<li id="hk'+ underKatId +'" onclick="visaUKProdukter('+ underKatId +')"><a class="dropdown-item" href="#">'+ underKatSub +'</a></li>';
+            /*var underHuvudKat = '<li id="uk'+ underKatId +'" onclick="visaUKProdukter('+ underKatId +')"><a href="#">'+ underKatPack +'</a></li>';
+            
+            var underHuvudKat = '<li class="dropdown-item" id="uk'+ underKatId +'" onclick="visaUKProdukter('+ underKatId +')"><a href="#">'+ underKat[i].subcategory +'</a></li>';
             */
-            var underHuvudKat = '<a class="dropdown-item" href="#" onclick="visaUKProdukter('+ underKatId +'id="hk'+ underKatId +'"</a>';
-
+            var underHuvudKat = '<a class="dropdown-item" href="#" onclick="visaUKProdukter('+ underKatId +'id="uk'+ underKat[i].subcategory +'>'+ underKatSub +'</a>';
+            
             if (underKatHuvud == 1) {
                 $('#hk1').append(underHuvudKat);
             
@@ -121,6 +123,37 @@ fetch("assets/underkategorier.json")
                         
                     };
                 };
+                
+                visaUKProdukter = function(val){
+                    
+                            $(".main").empty();
+                    
+                            var value = val;
+                            
+                    
+                            for(i = 0; i < produkter.length; i++) {
+                    
+                                var produktId = (itemsData[i].id);
+                                var produktName = (itemsData[i].prodName);
+                                var produktDesc = (itemsData[i].prodDesc);
+                                var produktImage = "images/" + (itemsData[i].image);
+                                var produktPrice = (itemsData[i].prodPrice);
+                                var produktHK = (itemsData[i].huvudKat);
+                                var produktUK = (itemsData[i].underKat);
+                                var produktCard = "";
+                    
+                                var produktCard = '<div class="col-lg-3"><div class="card-deck"><div class="card text-center"><img class="card-img-top" src="' + produktImage + '"><div class="card-body"><h4 class="card-title">' + produktName + '</h4><div class="card-text"><p>' + produktPrice + ' kr</p><div class="btn btn-light"><a href="#" >SHOP NOW</a></div></div></div></div></div></div>';
+                    
+                                
+                                // Skriv en IF sats som kollar att det bara skrivs ut rätt produkter
+                                if ( produktUK == value) {
+                                
+                                //$('.allProducts').html(" ");
+                                $('.main').append(produktCard);
+                                }
+                                
+                            };
+                        };
 
     
     };
